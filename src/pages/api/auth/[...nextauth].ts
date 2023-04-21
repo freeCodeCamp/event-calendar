49124c28f5e90f8dff8fd36d60c296c20aa5985c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-04-21 18:13:36 +0200

    feat(auth): add next-auth login

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+// TODO: create a env.ts file to validate the env variables
+if (!process.env.AUTH0_CLIENT_ID) throw new Error("AUTH0_CLIENT_ID is not set");
+if (!process.env.AUTH0_CLIENT_SECRET)
+  throw new Error("AUTH0_CLIENT_SECRET is not set");
+
+export const authOptions = {
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
