76dd53c937e65e4f34eab44e0abeb749eefe65ad
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-04-24 12:05:19 +0200

    feat: add prisma adapter for user account creation

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import { prisma } from "@/db";
 
 // TODO: create a env.ts file to validate the env variables
 if (!process.env.AUTH0_CLIENT_ID) throw new Error("AUTH0_CLIENT_ID is not set");
@@ -7,6 +10,7 @@ if (!process.env.AUTH0_CLIENT_SECRET)
   throw new Error("AUTH0_CLIENT_SECRET is not set");
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
