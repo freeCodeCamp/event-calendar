5cbd1cbeaf4bd9734027095446ea149bb97d17f8
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-04-26 17:57:51 +0200

    fix: tighten csp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // Eval is used in development, so we have to allow it.
-const CSP = `default-src 'self'; style-src 'self' 'unsafe-inline'; ${
+const CSP = `default-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; ${
   process.env.NODE_ENV === "development"
     ? "script-src 'self' 'unsafe-eval'"
     : ""
