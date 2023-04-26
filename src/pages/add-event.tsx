78ee5bb8e877c9efc3d642ae294ae69e91f155a6
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-04-26 15:29:40 +0200

    feat: redirect to home after event creation
    Also refacto to use react-hook-form for the nice DX.

--- src/pages/add-event.tsx
+++ src/pages/add-event.tsx
@@ -1,20 +1,50 @@
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+type EventData = {
+  name: string;
+  link: string;
+  date: string;
+};
+
 export default function AddEvent() {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<EventData>();
+
+  const postEvent = async (event: EventData) => {
+    const res = await fetch("/api/event", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(event),
+    });
+
+    if (res.ok) {
+      router.push("/");
+    } else {
+      throw new Error("Something went wrong");
+    }
+  };
+
   return (
-    <form action="/api/event" method="post">
+    <form onSubmit={handleSubmit(postEvent)}>
       <div>
-        <label htmlFor="name">Name: </label>
-        <input type="text" name="name" id="name" required />
+        <label htmlFor="name">Event Name: </label>
+        <input type="text" required {...register("name", { required: true })} />
       </div>
       <div>
         <label htmlFor="link">Url: </label>
-        <input type="url" name="link" id="link" required />
+        <input type="url" required {...register("link", { required: true })} />
       </div>
       <div>
         <label htmlFor="date">Date: </label>
-        <input type="date" name="date" id="date" required />
+        <input
+          type="datetime-local"
+          required
+          {...register("date", { required: true })}
+        />
       </div>
       <div>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Create Event" />
       </div>
     </form>
   );
