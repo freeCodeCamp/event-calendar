e61adeac4b0ed2ac58f8ceb2ef6f244e73557121
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-04-26 17:17:35 +0200

    fix: add CSP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
+// Eval is used in development, so we have to allow it.
+const CSP = `default-src 'self'; style-src 'self' 'unsafe-inline'; ${
+  process.env.NODE_ENV === "development"
+    ? "script-src 'self' 'unsafe-eval'"
+    : ""
+}`;
+
+// TODO: add the rest.
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: CSP,
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
