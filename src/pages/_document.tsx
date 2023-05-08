833a1ec68b2cc9cab8faf7bafcde97f5c7cb024a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-05-08 11:34:11 +0200

    fix: move meta into _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Tech events" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
