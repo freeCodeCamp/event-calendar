900c9604bff17e1910148a35ea677a8be23945ae
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-05-10 12:54:55 +0200

    fix: add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,11 @@ import type { AppProps } from "next/app";
 
 import Layout from "@/components/layout";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
