fe920cadf9307ee2cf09c25de14507693212f66a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-05-15 10:19:24 +0200

    fix: expand CSP for production
    It now needs  script-src: unsafe-inline in production

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const CSP = `default-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; connect-src 'self' https://ipwho.is; ${
   process.env.NODE_ENV === "development"
     ? "script-src 'self' 'unsafe-inline' 'unsafe-eval'"
-    : ""
+    : "script-src 'self' 'unsafe-inline'" // TODO: Is there a safer way to do this? Nonce?
 }`;
 
 // TODO: add the rest.
