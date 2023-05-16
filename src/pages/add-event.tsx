98329416730159c5097e4bff473586f5a2cd089a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-05-16 12:30:08 +0200

    feat: use MUI for add-event form

--- src/pages/add-event.tsx
+++ src/pages/add-event.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import { typeboxResolver } from "@hookform/resolvers/typebox";
+import { Paper, Button, Stack, TextField, Typography } from "@mui/material";
 
 import { type EventData, eventSchema } from "@/validation/schema";
 
@@ -29,73 +30,84 @@ export default function AddEvent() {
   };
 
   return (
-    <form onSubmit={handleSubmit(postEvent)}>
-      <div>
-        <label htmlFor="name">Event Name: </label>
-        <input
-          data-cy="input-name"
-          type="text"
-          required
-          {...register("name", { required: true })}
-        />
-        {errors.name && (
-          <span>Event names must be between 1 and 100 characters long</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="link">Url: </label>
-        <input
-          data-cy="input-url"
-          type="url"
-          required
-          {...register("link", { required: true })}
-        />
-        {errors.link && (
-          <span>URLs must include the protocol (http:// or https://)</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="date">Date: </label>
-        <input
-          data-cy="input-date"
-          type="datetime-local"
-          required
-          {...register("date", {
-            required: true,
-            setValueAs: (value) => (value ? new Date(value).toISOString() : ""),
-          })}
-        />
-        {errors.date && <span>Dates must be ISO8601 compliant</span>}
-      </div>
-      <div>
-        <label htmlFor="latitude">Latitude: </label>
-        <input
-          data-cy="input-latitude"
-          type="text"
-          required
-          {...register("latitude", { required: true, valueAsNumber: true })}
-        />
-        {errors.latitude && (
-          <span>Latitude must be between -90 and 90 inclusive</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="longitude">Longitude: </label>
-        <input
-          data-cy="input-longitude"
-          type="text"
-          required
-          {...register("longitude", { required: true, valueAsNumber: true })}
-        />
-        {errors.longitude && (
-          <span>Longitude must be between -180 and 180 inclusive</span>
-        )}
-      </div>
-      <div>
-        <button data-cy="submit-add-event" type="submit">
-          Create Event
-        </button>
-      </div>
-    </form>
+    <Paper
+      variant="outlined"
+      sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+    >
+      <form onSubmit={handleSubmit(postEvent)}>
+        <Typography variant="h6" gutterBottom>
+          Add event
+        </Typography>
+        <Stack spacing={2}>
+          <TextField
+            data-cy="input-name"
+            type="text"
+            required
+            label="Event Name"
+            error={!!errors.name}
+            helperText={
+              errors.name &&
+              "Event names must be between 1 and 100 characters long"
+            }
+            {...register("name", { required: true })}
+          />
+
+          <TextField
+            data-cy="input-url"
+            type="url"
+            required
+            label="URL"
+            error={!!errors.link}
+            helperText={
+              errors.link &&
+              "URLs must include the protocol (http:// or https://)"
+            }
+            {...register("link", { required: true })}
+          />
+
+          <TextField
+            data-cy="input-date"
+            type="datetime-local"
+            required
+            label="Date"
+            error={!!errors.date}
+            helperText={errors.date && "Dates must be ISO8601 compliant"}
+            {...register("date", {
+              required: true,
+              setValueAs: (value) =>
+                value ? new Date(value).toISOString() : "",
+            })}
+          />
+
+          <TextField
+            data-cy="input-latitude"
+            type="text"
+            required
+            label="Latitude"
+            error={!!errors.latitude}
+            helperText={
+              errors.latitude && "Latitude must be between -90 and 90 inclusive"
+            }
+            {...register("latitude", { required: true, valueAsNumber: true })}
+          />
+          <TextField
+            data-cy="input-longitude"
+            type="text"
+            required
+            label="Longitude"
+            error={!!errors.longitude}
+            helperText={
+              errors.longitude &&
+              "Longitude must be between -180 and 180 inclusive"
+            }
+            {...register("longitude", { required: true, valueAsNumber: true })}
+          />
+
+          <Button data-cy="submit-add-event" type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Paper>
   );
 }
